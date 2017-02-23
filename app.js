b904a5dabcab8a4665ebad4cf5c154417f4ef22f
Daniel Vega <danve4@gmail.com>
2017-02-23 17:07:56 -0600

    Render the test file

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
