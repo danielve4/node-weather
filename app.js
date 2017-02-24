7bb876bf5f617bb9f281a5687bac48e6a46fffb7
Daniel Vega <danve4@gmail.com>
2017-02-23 19:41:43 -0600

    Corrected way to send index.html

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// // views is directory for all template files
+// views is directory for all template files
 // app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
+// app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
-  response.render('index.html');
+  response.sendFile(__dirname+'/index.html');
 });
 
 app.listen(app.get('port'), function() {
