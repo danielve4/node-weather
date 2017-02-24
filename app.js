d6fa3540eb14d706ee769f63c8ec9ad84d7a07e1
Daniel Vega <danve4@gmail.com>
2017-02-24 04:07:14 -0600

    Simplified the way to get weather data

--- app.js
+++ app.js
@@ -8,53 +8,52 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views');
 
-var httpget = function(query, callback) {
+function httpget(query, callback) {
   https.get(query, function(res) {
     var body = '';
     res.on('data', function(chunk) {
       body += chunk;
     });
-
     res.on('end', function() {
       body = JSON.parse(body);
       callback(body);
     });
-
     res.on('error', function (error) {
       callback('Error: ' + error.message);
     });
   });
-};
+}
+
+function getCoordinates(address, callback) {
+  var mapsKey = 'AIzaSyBumPhCSIrrBtwTIbeZZ5mdW7tNa_s5FXA';
+  var addressQuery = 'https://maps.googleapis.com/maps/api/geocode/json?address='+address+
+    '&key='+mapsKey;
+  httpget(addressQuery, callback);
+}
+
+function getWeather(lat, lng, callback) {
+  var weatherKey = '10ec48a74229fb3f53027bee3f2bfb2b';
+  var weatherQuery = 'https://api.darksky.net/forecast/'+weatherKey+'/'+
+    lat+','+lng+'?exclude=minutely,hourly,flags';
+  httpget(weatherQuery, callback);
+}
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname+'/views/index.html');
 });
 
-app.get('/coordinates/:address', function(request, response) {
+app.get('/weather/:address',function (request, response) {
   var address = request.params.address;
-  var mapsKey = 'AIzaSyBumPhCSIrrBtwTIbeZZ5mdW7tNa_s5FXA';
-  var addressQuery = 'https://maps.googleapis.com/maps/api/geocode/json?address='+address+
-    '&key='+mapsKey;
-  httpget(addressQuery, function(data) {
-    if(JSON.stringify(data).startsWith('Error')) {
-      response.status(404).send(data);
+  getCoordinates(address, function(data) {
+    if(data.status === "OK") {
+      var loc = data.results[0].geometry.location;
+      if (loc.lat !== '' && loc.lng !== '') {
+        getWeather(loc.lat, loc.lng, function(data) {
+          response.send(data);
+        })
+      }
     } else {
-      response.send(data);
-    }
-  });
-});
-
-app.get('/weather/:lat,:lng', function(request, response){
-  var lat = request.params.lat;
-  var lng = request.params.lng;
-  var weatherKey = '10ec48a74229fb3f53027bee3f2bfb2b';
-  var weatherQuery = 'https://api.darksky.net/forecast/'+weatherKey+'/'+
-    lat+','+lng+'?exclude=minutely,hourly,flags';
-  httpget(weatherQuery, function(data) {
-    if(JSON.stringify(data).startsWith('Error')) {
       response.status(404).send(data);
-    } else {
-      response.send(data);
     }
   });
 });
