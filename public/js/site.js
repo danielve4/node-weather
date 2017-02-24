d6c857cf102f7dd5afa1b440bf7d8dad308caec1
Daniel Vega <danve4@gmail.com>
2017-02-23 22:34:33 -0600

    Simplified getting the coordinates fo an address

--- public/js/site.js
+++ public/js/site.js
@@ -1,8 +1,9 @@
+//TODO Create a error function to handle any errors
 $.noConflict();
 (function($) {
   $(document).ready(function() {
 
-    let getWeatherInfo = function(lat,lng, callback) {
+    var getWeatherInfo = function(lat,lng, callback) {
       $.when($.ajax({
         type: 'GET',
         url: 'https://api.darksky.net/forecast/10ec48a74229fb3f53027bee3f2bfb2b/'+lat+','+lng+
@@ -12,41 +13,35 @@ $.noConflict();
       });
     };
 
-    let getWeather = function(loc) {
+    var getWeather = function(loc) {
       if(loc.lat!=='' && loc.lng!=='') {
         getWeatherInfo(loc.lat, loc.lng, function(data) {
-          let current = data.currently;
-          let curTemp = current.temperature;
+          var current = data.currently;
+          var curTemp = current.temperature;
           console.log(curTemp);
         });
       }
     };
 
-    let getAddressInfo = function(address, callback) {
-      $.when($.ajax({
-        type: 'GET',
-        url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+address+
-        '&key=AIzaSyBumPhCSIrrBtwTIbeZZ5mdW7tNa_s5FXA'
-      })).then(callback, function() {
-        console.log("Error");
-      });
-    };
-
-    let getCoordinates = function(callback) {
-      let address = ($('#w-text').val());
+    var getCoordinates = function(callback) {
+      var address = ($('#w-text').val());
       if(address.length > 0) {
-        getAddressInfo(address, function(data) {
+        $.when($.ajax({
+          type: 'GET',
+          url: '/coordinates/'+address
+        })).then(function(data) {
           if(data.status === "OK") {
-            let loc = data.results[0].geometry.location;
+            var loc = data.results[0].geometry.location;
             callback(loc);
           }
+        }, function() {
+          console.log("Error");
         });
       }
     };
 
     $('#w-form').on('submit', function(e) {
       getCoordinates(getWeather);
-
       e.preventDefault();
     });
 
