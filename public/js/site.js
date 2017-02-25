a7bc9ec2e8d79068f9c21818937e792fbb4d7493
Daniel Vega <danve4@gmail.com>
2017-02-25 05:21:40 -0600

    Adds the cards with the data

--- public/js/site.js
+++ public/js/site.js
@@ -1,9 +1,56 @@
 $.noConflict();
 jQuery(function($) {
+  var weekDays = [
+    'Sunday','Monday','Tueday',
+    'Wednesday','Thursday','Friday','Saturday'
+  ];
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+  var todayDate;
   $(document).ready(function() {
-
     function addCurrentWeatherChart(weatherData) {
+      $('#weather-data').empty();
       console.log(weatherData);
+      var today = formattedDay();
+      var current = weatherData.current;
+
+      $('#weather-data').append(
+        '<li class="card">'+
+          '<ul id="current">'+
+            '<li id="current-date">'+
+               today.month + ' ' + today.day +
+            '</li>' +
+            '<li id="current-temperature">'+
+              Math.round(current.currently.temperature) + '° F' +
+            '</li>' +
+            '<li>' +
+              'Currently: '+current.currently.summary +
+            '</li>'+
+          '</ul>'+
+        '</li>'+
+        '<li class="card">'+
+          '<ul id="forecast">'+
+          '</ul>'+
+        '</li>'+
+        '<li class="ct-chart ct-perfect-fourth card"></li>'
+      );
+
+      for(var i=0;i<current.nextDays.daily.length;i++) {
+        $('#forecast').append(
+          '<li class="forecast-day ">' +
+          current.nextDays.daily[i].day + ' ' +
+          '<span class="forecast-high-low">' +
+          Math.round(current.nextDays.daily[i].temperatureMax) + '°/' +
+          Math.round(current.nextDays.daily[i].temperatureMin) + '°' +
+          '</span>' +
+          '</li>'
+        );
+      }
+
       var chartTemp = {
         labels: weatherData.current.forecastChart.days,
         series: [
@@ -15,12 +62,11 @@ jQuery(function($) {
     }
 
     function getWeather(address, callback) {
-      var from = Math.floor(Date.now() / 1000);
-      console.log(from);
+      todayDate = Math.floor(Date.now() / 1000);
       var days = 3;
       $.when($.ajax({
         type: 'GET',
-        url: '/weather/'+address+','+days+','+from
+        url: '/weather/'+address+','+days+','+todayDate
       })).then(function(data) {
         callback(data);
       }, failure);
@@ -44,6 +90,20 @@ jQuery(function($) {
     });
 
   });
+
+  function formattedDay() {
+    var date = new Date(todayDate*1000);
+    var day = date.getDate();
+    var month = date.getMonth();
+    var hourMinutes = date.getHours() +':'+ date.getMinutes();
+    var dayFormat = {
+      'day':day,
+      'month':monthNames[month],
+      'time':hourMinutes
+    };
+
+    return dayFormat;
+  }
 
   function failure(error) {
     if(error.length>0) {
