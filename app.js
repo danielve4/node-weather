d7146a03df05f6b385d5e198180c05977ed3fd13
Daniel Vega <danve4@gmail.com>
2017-02-25 01:25:17 -0600

    Deleted comment

--- app.js
+++ app.js
@@ -15,20 +15,6 @@ app.get('/', function(request, response) {
 
 app.use('/weather', weather);
 
-//
-// app.get('/past-days/:numDays,:from', function(resquest, response) {
-//   var numPreviousDays = request.params.numDays;
-//   var today = new Date(request.params.from*1000);
-//   var dayInSeconds = 24*60*60;
-//   var daysToQuery = [];
-//   for(var j=1;j<=numPreviousDays;j++) {
-//     daysToQuery.push(today-(dayInSeconds*j));
-//   }
-//
-//
-//
-// });
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
